08a4d540638816785d427e326057d7ef186c6891
ngmh <Ngmhcartoon@gmail.com>
2017-10-23 00:17:23 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -6,19 +6,9 @@
 	<body>
 		<p id="header">A random 2D platformer game made without canvas (yes I know this is really bad I'll convert it to canvas really soon i promise!)</p>
 		<canvas id="screen">HTML5 Canvas is not supported by your browser.</canvas></br>
-		<button id="source" onclick="alert('Uploaded Soon, No Wifi now');">Source Code</button>
+		<a id="source" href="https://github.com/ngmhprogramming/2plat">Source Code</a>
 		<p id="codes">Some extra block codes for developers: A, Air, B, Brick, C, Coin, D, Deadly, E, Entrance, P, Player</p>
-		<p>The Chinese National Anthem... Because Why Not?</p>
-		<p>起来不愿做奴隶的人们</br>
-		   把我们的血肉筑成我们新的长城</br>
-		   中华民族到了最危险的时候</br>
-		   每个人被迫着发出最后的吼声</br>
-		   起来起来起来</br>
-	   	   我们万众一心</br>
-		   冒着敌人的炮火前进</br>
-		   冒着敌人的炮火前进</br>
-		   前进前进进</br>
-		</p>
+		<p>Contact me at ngmhcartoon@gmail.com if you have a level plan to submit.</p>
 	</body>
 	<link type="text/css" href="style.css" rel="stylesheet">
 	<script type="text/javascript" src="game.js"></script>
